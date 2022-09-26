12b1170adff58423c739a46ce9d69ce2b92c7c16
csantosm <4a.santos@gmail.com>
2022-09-26 10:21:10 +0200

    ov-components-react: Added project structure

---
+++ openvidu-components-react/openvidu-components-testapp/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import { Button } from 'openvidu-react';
+
+function App() {
+    return (
+        <div className="App">
+            <Button>ALAL</Button>
+        </div>
+    );
+}
+
+export default App;
